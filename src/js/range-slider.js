12f2ce2f6d777ab22eda5cceb28a0b34ffee6cfc
Andrey Sery <andrey.72006@gmail.com>
2022-11-01 00:33:33 +0300

    Add libs, styling nouislider

--- src/js/range-slider.js
+++ src/js/range-slider.js
@@ -1,15 +1,36 @@
-import noUiSlider from 'nouislider';
+import '../libs/nouislider/nouislider.min.js';
 
 const slider = document.getElementById('range-slider');
 
 if (slider) {
   noUiSlider.create(slider, {
-    start: [0, 30000],
+    start: [1500, 30000],
     connect: true,
     step: 1,
     range: {
       min: [0],
-      max: [30000],
+      max: [35000],
     },
   });
+
+  const inputOne = document.getElementById('input01');
+  const inputTwo = document.getElementById('input02');
+  const inputs = [inputOne, inputTwo];
+  
+  slider.noUiSlider.on('update', (values, handle) => {
+    inputs[handle].value = Math.round(values[handle]);
+  });
+  
+  const setSlider = (i, value) => {
+    let arr = [null, null];
+    arr[i] = value;
+    slider.noUiSlider.set(arr);
+  };
+  
+  inputs.forEach((item, index) => {
+    item.addEventListener('change', (e) => {
+      setSlider(index, e.currentTarget.value);
+    });
+  });
 }
+
